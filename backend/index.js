3ab44e165fcb56fc99e725a7af57297dda9ddc25
Deepadharsini <dharsinikannan.2112@gmail.com>
2025-05-03 16:33:22 +0530

    edit

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/api", uploadRoutes);
 app.use("/api", downloadRoutes);
 
-app.get("/", (req, res) => res.send("Filefulse is running.."));
+app.get("/", (req, res) => res.send("Filefuse is running.."));
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on http://localhost:${process.env.PORT}`)
